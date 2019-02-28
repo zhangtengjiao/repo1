aeadbdc525e9bb3d1dc80eda2eed784f5afe82c8
Jon <1020061308@qq.com>
2019-02-28 19:06:25 +0800

    添加到本地版本管理库2

---
+++ git-test01/src/main/java/cn/bfcat/git/FirstClass.java
@@ -0,0 +1,16 @@
+package cn.bfcat.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaaaaa");
+
+
+    }
+}
